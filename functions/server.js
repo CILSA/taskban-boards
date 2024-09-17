ef2594483bc1f2b0fde6e60594a3be31e4ec7b59
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-09-17 10:47:52 -0300

    router en app

--- functions/server.js
+++ functions/server.js
@@ -2,11 +2,19 @@
 import express from "express";
 import cors from "cors";
 import serverless from "serverless-http";
-import routes from "./routes/routes.js";
+import express from "express";
+import { fetchBoards, fetchBoardById, addBoard } from "./controllers/controller.js";
 
 const app = express();
 app.use(cors());
+const router = express.Router();
 
-app.use('/.netlify/functions/server', routes);
+router.get("/boards", fetchBoards); // Obtener todos los boards o filtrar por ID
+router.get("/boards/:id", fetchBoardById); // Obtener un board específico
+router.post("/boards", addBoard); // Crear un nuevo board
+
+
+
+app.use('/.netlify/functions/server', router);
 
 export const handler = serverless(app);
