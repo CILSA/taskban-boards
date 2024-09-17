fbcd9e207e57697a94386b18524a6a648cf4b18e
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-09-17 16:36:25 -0300

    agrego eliminar y editar

--- functions/server.js
+++ functions/server.js
@@ -2,7 +2,13 @@
 import express from "express";
 import cors from "cors";
 import serverless from "serverless-http";
-import { fetchBoards, fetchBoardById, addBoard } from "./controllers/controller";
+import {
+  fetchBoards,
+  fetchBoardById,
+  addBoard,
+  updateBoardById,  // Importar la función de actualización
+  deleteBoardById   // Importar la función de eliminación
+} from "./controllers/controller";
 
 const app = express();
 app.use(cors());
@@ -12,8 +18,8 @@ const router = express.Router();
 router.get("/boards", fetchBoards); // Obtener todos los boards o filtrar por ID
 router.get("/boards/:id", fetchBoardById); // Obtener un board específico
 router.post("/boards", addBoard); // Crear un nuevo board
-
-
+router.put("/boards/:id", updateBoardById);
+router.delete("/boards/:id", deleteBoardById);
 
 app.use('/.netlify/functions/server', router);
 
